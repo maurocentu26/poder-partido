b40b676514399c2ebb12bc26486ce715d1ae9a4f
Mauro Centurion <87332552+maurocentu26@users.noreply.github.com>
2024-12-09 15:26:35 -0300

    Adding the new index and javascript files

---
+++ app.js
@@ -0,0 +1,15 @@
+const acc = document.getElementsByClassName("acord__btn");
+
+console.log(acc);   
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
